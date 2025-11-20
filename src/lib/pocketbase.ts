d14ae1b451ade833fef199f3c989f01b2ec136ac
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2025-11-20 01:20:51 +0100

    test

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,10 +1,11 @@
-// src/lib/pocketbase.ts
 import PocketBase from 'pocketbase';
-
-// URL de ton instance PocketBase
-// Utilise la variable d'environnement ou fallback sur localhost
-const POCKETBASE_URL = import.meta.env.PUBLIC_POCKETBASE_URL || 'https://portfolio.victor-landwerlin.fr:443';
-export const pb = new PocketBase(POCKETBASE_URL);
+import type { TypedPocketBase } from "./pocketbase-types";
+var path='';
+if(import.meta.env.MODE === 'development')
+    path = 'http://localhost:8090'    //localhost = machine de dev
+else path = 'https://portfolio.victor-landwerlin.fr:443'   //url du site 
+const pb = new PocketBase(path) as TypedPocketBase;
+export default pb;
 
 // Type pour les projets
 export interface Project {
